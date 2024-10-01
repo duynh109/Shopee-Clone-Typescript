5ef2214cc82c034865a692629790c8e327d45bd2
duynh109 <duy10092002@gmail.com>
2024-10-01 19:37:50 +0700

    Code UI Register Layout

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,288 @@
+export default function Footer() {
+  return (
+    <footer className='py-14 bg-neutral-100'>
+      <div className='max-w-7xl mx-auto px-4'>
+        <div className='flex items-start text-xs'>
+          <div className='flex-1 p-[0.3125rem]'>
+            <div className='font-bold mb-5'>CHĂM SÓC KHÁCH HÀNG</div>
+            <ul className='list-none m-0 mb-4 p-0 text-black/65'>
+              <li className='block mb-3'>Trung Tâm Trợ Giúp</li>
+              <li className='block mb-3'>Shopee Blog</li>
+              <li className='block mb-3'>Shopee Mall</li>
+              <li className='block mb-3'>Hướng Dẫn Mua Hàng</li>
+              <li className='block mb-3'>Hướng Dẫn Bán Hàng</li>
+              <li className='block mb-3'>Thanh Toán</li>
+              <li className='block mb-3'>Shopee Xu</li>
+              <li className='block mb-3'>Vận Chuyển</li>
+              <li className='block mb-3'>Trả Hàng & Hoàn Tiền</li>
+              <li className='block mb-3'>Chăm Sóc Khách Hàng</li>
+              <li className='block mb-3'>Chính Sách Bảo Hành</li>
+            </ul>
+          </div>
+          <div className='flex-1 p-[0.3125rem]'>
+            <div className='font-bold mb-5'>VỀ SHOPEE</div>
+            <ul className='list-none m-0 mb-4 p-0 text-black/65'>
+              <li className='block mb-3'>Giới Thiệu Về Shopee Việt Nam</li>
+              <li className='block mb-3'>Tuyển Dụng</li>
+              <li className='block mb-3'>Điều Khoản Shopee</li>
+              <li className='block mb-3'>Chính Sách Bảo Mật</li>
+              <li className='block mb-3'>Chính Hãng</li>
+              <li className='block mb-3'>Kênh Người Bán</li>
+              <li className='block mb-3'>Flash Sales</li>
+              <li className='block mb-3'>Chương Trình Tiếp Thị Liên Kết Shopee</li>
+              <li className='block mb-3'>Liên Hệ Với Truyền Thông</li>
+            </ul>
+          </div>
+          <div className='flex-1 p-[0.3125rem]'>
+            <div className='font-bold mb-5'>THANH TOÁN</div>
+            <ul className='flex flex-wrap list-none m-0 mb-4 p-0 text-black/65'>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-md h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/d4bbea4570b93bfd5fc652ca82a262a8'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-md h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/a0a9062ebe19b45c1ae0506f16af5c16'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-md h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/38fd98e55806c3b2e4535c4e4a6c4c08'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-md h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/bc2a874caeee705449c164be385b796c'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-md h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/2c46b83d84111ddc32cfd3b5995d9281'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-md h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/5e3f0bee86058637ff23cfdf2e14ca09'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-md h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/9263fa8c83628f5deff55e2a90758b06'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-md h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/0217f1d345587aa0a300e69e2195c492'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+            </ul>
+            <div className='font-bold mb-5'>ĐƠN VỊ VẬN CHUYỂN</div>
+            <ul className='flex flex-wrap list-none m-0 mb-4 p-0 text-black/65'>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-md h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/vn-50009109-159200e3e365de418aae52b840f24185'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-md h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/d10b0ec09f0322f9201a4f3daf378ed2'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-md h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/vn-50009109-64f0b242486a67a3d29fd4bcf024a8c6'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-md h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/59270fb2f3fbb7cbc92fca3877edde3f'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-md h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/957f4eec32b963115f952835c779cd2c'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-md h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/0d349e22ca8d4337d11c9b134cf9fe63'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-md h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/3900aefbf52b1c180ba66e5ec91190e5'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-md h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/6e3be504f08f88a15a28a9a447d94d3d'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-md h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/b8348201b4611fc3315b82765d35fc63'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-sm h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/0b3014da32de48c03340a4e4154328f6'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+              <li className='flex bg-white p-[0.3rem] rounded-[0.125rem] shadow-sm h-8 w-16 mb-2 mr-2'>
+                <a>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/vn-50009109-ec3ae587db6309b791b78eb8af6793fd'
+                    className='w-full h-full'
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className='flex-1 p-[0.3125rem]'>
+            <div className='font-bold mb-5'>THEO DÕI CHÚNG TÔI TRÊN</div>
+            <ul className='list-none m-0 mb-4 p-0 text-black/65'>
+              <li className='block mb-3'>
+                <a href='https://www.facebook.com/ShopeeVN' className='flex'>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/2277b37437aa470fd1c71127c6ff8eb5'
+                    className='mr-2'
+                  />
+                  <span>Facebook</span>
+                </a>
+              </li>
+              <li className='block mb-3'>
+                <a href='https://www.instagram.com/Shopee_VN' className='flex'>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/5973ebbc642ceee80a504a81203bfb91'
+                    className='mr-2'
+                  />
+                  <span>Instagram</span>
+                </a>
+              </li>
+              <li className='block mb-3'>
+                <a href='https://www.linkedin.com/company/shopee/' className='flex'>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/f4f86f1119712b553992a75493065d9a'
+                    className='mr-2'
+                  />
+                  <span>LinkedIn</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className='flex-1 p-[0.3125rem]'>
+            <div className='font-bold mb-5'>TẢI ỨNG DỤNG SHOPEE NGAY THÔI</div>
+            <div className='flex w-full flex-row'>
+              <a href='https://shopee.vn/web'>
+                <img
+                  src='https://down-vn.img.susercontent.com/file/a5e589e8e118e937dc660f224b9a1472'
+                  className='bg-white shadow-md w-24 h-24 p-1 mr-3'
+                />
+              </a>
+              <div className='inline-block'>
+                <a href='https://shopee.vn/web' className='flex bg-white rounded-sm mb-2 h-[26px] p-1 shadow-md'>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/ad01628e90ddf248076685f73497c163'
+                    className='w-full h-full'
+                  />
+                </a>
+                <a href='https://shopee.vn/web' className='flex bg-white rounded-sm mb-2 h-[26px] p-1 shadow-md'>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/ae7dced05f7243d0f3171f786e123def'
+                    className='w-full h-full'
+                  />
+                </a>
+                <a href='https://shopee.vn/web' className='flex bg-white rounded-sm mb-2 h-[26px] p-1 shadow-md'>
+                  <img
+                    src='https://down-vn.img.susercontent.com/file/35352374f39bdd03b25e7b83542b2cb0'
+                    className='w-full h-full'
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 border-t border-gray-300/100 py-10'>
+          <div className='lg:col-span-1'>
+            <div>© 2024 Shopee. Tất cả các quyền được bảo lưu.</div>
+          </div>
+          <div className='lg:col-span-2'>
+            <div>
+              Quốc gia & Khu vực: Singapore | Indonesia | Thái Lan | Malaysia | Việt Nam | Philippines | Brazil | México
+              | Colombia | Chile | Đài Loan
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='max-w-7xl mx-auto px-4'>
+        <div className='text-center text-sm mt-10'>
+          <div className='mb-6'>Công ty TNHH Shopee</div>
+          <div className='mt-2'>
+            Địa chỉ: Tầng 4-5-6, Tòa nhà Capital Place, số 29 đường Liễu Giai, Phường Ngọc Khánh, Quận Ba Đình, Thành
+            phố Hà Nội, Việt Nam. Tổng đài hỗ trợ: 19001221 - Email: cskh@hotro.shopee.vn
+          </div>
+          <div className='mt-2'>Chịu Trách Nhiệm Quản Lý Nội Dung: Nguyễn Bùi Anh Tuấn</div>
+          <div className='mt-2'>
+            Mã số doanh nghiệp: 0106773786 do Sở Kế hoạch & Đầu tư TP Hà Nội cấp lần đầu ngày 10/02/2015
+          </div>
+          <div className='mt-2'>© 2015 - Bản quyền thuộc về Công ty TNHH Shopee</div>
+        </div>
+      </div>
+    </footer>
+  )
+}
