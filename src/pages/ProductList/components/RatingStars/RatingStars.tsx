08314eec9ecc936799771ae2f13a9fbf6b02d8d2
duynh109 <duy10092002@gmail.com>
2024-12-18 22:33:13 +0700

    Refactor code đưa các component vào folder ProductList/components

--- src/pages/ProductList/components/RatingStars/RatingStars.tsx
+++ src/pages/ProductList/components/RatingStars/RatingStars.tsx
@@ -1,6 +1,6 @@
 import { createSearchParams, useNavigate } from 'react-router-dom'
-import path from '../../../constants/path'
-import { QueryConfig } from '../ProductList'
+import path from '../../../../constants/path'
+import { QueryConfig } from '../../ProductList'
 
 /**
  * index 0: Có 5 sao màu vàng tương ứng từ indexStar 0 - 4 đều màu vàng
